39159cc5208b1fb86cb083e7129236fb15155f02
Jihoon Lee <jhoon.it.lee@samsung.com>
2021-06-17 18:48:00 +0900

    [LayerNode] Add layer(devel) to the node
    **Changes proposed in this PR:**
    - Add `Layer` to the node
    - Remove `node_exporter.h` from `layer_node.h`
    - Change compile time switch to runtime switch of checking layer v1 and
    layer v2
    
    **Self evaluation:**
    1. Build test: [X]Passed [ ]Failed [ ]Skipped
    2. Run test: [X]Passed [ ]Failed [ ]Skipped
    
    Signed-off-by: Jihoon Lee <jhoon.it.lee@samsung.com>

--- nntrainer/layers/layer_node.h
+++ nntrainer/layers/layer_node.h
@@ -12,41 +12,72 @@
  * @todo   Add printPreset support
  */
 
-#define LAYER_V2 false
-
 #ifndef __LAYER_NODE_H__
 #define __LAYER_NODE_H__
+
+#include <memory>
+#include <tuple>
+#include <vector>
 
 #include <graph_node.h>
 #include <layer.h>
 #include <layer_context.h>
 #include <layer_internal.h>
-#include <node_exporter.h>
 
 namespace nntrainer {
+
+class Layer;
+
+class Exporter;
+enum class ExportMethods;
+
+namespace props {
+class Name;
+}
 
 /**
  * @class   LayerNode class
  * @brief   layer node class for the graph
  */
-class LayerNode : public ml::train::Layer, public GraphNode {
+class LayerNode final : public ml::train::Layer, public GraphNode {
 public:
   /**
-   * @brief     Default constructor
+   * @brief Default constructor
    */
-  LayerNode() : LayerNode(nullptr) {}
+  LayerNode() : LayerNode(std::shared_ptr<nntrainer::LayerV1>(nullptr)) {}
 
   /**
-   * @brief     Constructor of LayerNode class
+   * @brief Constructor of LayerNode class
+   * @todo  deprecate this
    *
    */
   LayerNode(std::shared_ptr<nntrainer::LayerV1> l, size_t idx = 0);
 
   /**
+   * @brief Constructor of LayerNode class for v2
+   * @param l layer to wrap with, the ownership is transferred to layer node
+   *
+   */
+  LayerNode(std::unique_ptr<nntrainer::Layer> &&l, size_t idx = 0);
+
+private:
+  /**
+   * @brief Construct a new Layer Node object
+   * @todo  deprecate this
+   *
+   * @param layer_v2 layer v2
+   * @param layer_v1 layer v1
+   * @param idx      idx
+   */
+  LayerNode(std::unique_ptr<nntrainer::Layer> &&layer_v2,
+            std::shared_ptr<nntrainer::LayerV1> layer_v1, size_t idx = 0);
+
+public:
+  /**
    * @brief     Destructor of LayerNode Class
    *
    */
-  ~LayerNode() = default;
+  ~LayerNode();
 
   /**
    * @brief     Set the index for the node
@@ -92,10 +123,7 @@ public:
    * @note      This name might be changed once this layer is added to the model
    * to keep the name unique to the model
    */
-  const std::string getName() const noexcept override {
-    auto &name = std::get<props::Name>(props);
-    return name.empty() ? "" : name.get();
-  }
+  const std::string getName() const noexcept override;
 
   /**
    * Support all the interface requirements by GraphNode<nntrainer::Layer>
@@ -216,7 +244,7 @@ public:
    */
   void addInputLayers(const std::string &in_layer) {
     input_layers.push_back(in_layer);
-    layer->setNumInputs(input_layers.size());
+    layerv1->setNumInputs(input_layers.size());
   }
 
   /**
@@ -226,7 +254,7 @@ public:
    */
   void addOutputLayers(const std::string &out_layer) {
     output_layers.push_back(out_layer);
-    layer->setNumOutputs(output_layers.size());
+    layerv1->setNumOutputs(output_layers.size());
   }
 
   /**
@@ -236,7 +264,7 @@ public:
    */
   void setInputLayers(const std::vector<std::string> &layers) {
     input_layers = layers;
-    layer->setNumInputs(layers.size());
+    layerv1->setNumInputs(layers.size());
   }
 
   /**
@@ -246,7 +274,7 @@ public:
    */
   void setOutputLayers(const std::vector<std::string> &layers) {
     output_layers = layers;
-    layer->setNumOutputs(layers.size());
+    layerv1->setNumOutputs(layers.size());
   }
 
   /**
@@ -254,7 +282,7 @@ public:
    * @return TensorDim dimension of the input
    */
   const std::vector<TensorDim> getInputDimensions() const {
-    if (LAYER_V2) {
+    if (layerv1 == nullptr) {
       return init_context.getInputDimensions();
     } else {
       return getLayer()->getInputDimension();
@@ -266,7 +294,7 @@ public:
    * @return TensorDim dimension of the output
    */
   const std::vector<TensorDim> getOutputDimensions() const {
-    if (LAYER_V2) {
+    if (layerv1 == nullptr) {
       return init_context.getOutputDimensions();
     } else {
       return getLayer()->getOutputDimension();
@@ -280,12 +308,7 @@ public:
    * @param exporter exporter that conatins exporting logic
    * @param method enum value to identify how it should be exported to
    */
-  virtual void
-  export_to(Exporter &exporter,
-            ExportMethods method = ExportMethods::METHOD_STRINGVECTOR) const {
-    exporter.saveResult(props, method, this);
-    layer->export_to(exporter, method);
-  }
+  void exportTo(Exporter &exporter, const ExportMethods &method) const;
 
   /**
    * @brief     read layer Weight & Bias data from file
@@ -309,11 +332,15 @@ public:
   friend std::ostream &operator<<(std::ostream &out, const LayerNode &l);
 
 private:
-  // TODO: make this unique_ptr once getObject API is removed
+  /// @todo: remove this
   std::shared_ptr<nntrainer::LayerV1>
+    layerv1; /**< The actual object in the graph node */
+
+  std::unique_ptr<nntrainer::Layer>
     layer; /**< The actual object in the graph node */
-  // TODO: possibly remove, two identifiers for the same  node (name and index)
-  // can lead to issues later
+
+  // TODO: possibly remove, two identifiers for the same node (name and
+  // index) can lead to issues later
   size_t index; /**< index of each node */
 
   /** TODO : move management of num_inputs to layer_node */
@@ -337,7 +364,8 @@ private:
    * These properties are set for the layer by the user but are intercepted
    * and used in the node which forms the basic element of the graph.
    */
-  std::tuple<props::Name> props; /**< properties for the layer node */
+  std::unique_ptr<std::tuple<props::Name>>
+    props; /**< properties for the layer node */
 
   /**
    * @brief setProperty by PropertyType
